78cb860b67536cc426c195b5f9a8f63b0ffb0691
Sumeet Padavala <sumeet.padavala@gmail.com>
2023-12-04 19:44:35 -0800

    Start project 5

--- p5/main.c
+++ p5/main.c
@@ -8,6 +8,7 @@
  */
 
 #include <signal.h>
+
 #include "system.h"
 
 /**
@@ -18,82 +19,86 @@
 static volatile int done;
 
 static void
-_signal_(int signum)
-{
-	assert( SIGINT == signum );
+_signal_(int signum) {
+    assert(SIGINT == signum);
 
-	done = 1;
+    done = 1;
 }
 
 double
-cpu_util(const char *s)
-{
-	static unsigned sum_, vector_[7];
-	unsigned sum, vector[7];
-	const char *p;
-	double util;
-	uint64_t i;
+cpu_util(const char *s) {
+    static unsigned sum_, vector_[7];
+    unsigned sum, vector[7];
+    const char *p;
+    double util;
+    uint64_t i;
 
-	/*
-	  user
-	  nice
-	  system
-	  idle
-	  iowait
-	  irq
-	  softirq
-	*/
+    /*
+      user
+      nice
+      system
+      idle
+      iowait
+      irq
+      softirq
+    */
 
-	if (!(p = strstr(s, " ")) ||
-	    (7 != sscanf(p,
-			 "%u %u %u %u %u %u %u",
-			 &vector[0],
-			 &vector[1],
-			 &vector[2],
-			 &vector[3],
-			 &vector[4],
-			 &vector[5],
-			 &vector[6]))) {
-		return 0;
-	}
-	sum = 0.0;
-	for (i=0; i<ARRAY_SIZE(vector); ++i) {
-		sum += vector[i];
-	}
-	util = (1.0 - (vector[3] - vector_[3]) / (double)(sum - sum_)) * 100.0;
-	sum_ = sum;
-	for (i=0; i<ARRAY_SIZE(vector); ++i) {
-		vector_[i] = vector[i];
-	}
-	return util;
+    vector[0] = rand();
+    vector[1] = rand();
+    vector[2] = rand();
+    vector[3] = rand();
+    vector[4] = rand();
+    vector[5] = rand();
+    vector[6] = rand();
+
+    if (!(p = strstr(s, " ")) ||
+        (7 != sscanf(p,
+                     "%u %u %u %u %u %u %u",
+                     &vector[0],
+                     &vector[1],
+                     &vector[2],
+                     &vector[3],
+                     &vector[4],
+                     &vector[5],
+                     &vector[6]))) {
+        return 0;
+    }
+    sum = 0.0;
+    for (i = 0; i < ARRAY_SIZE(vector); ++i) {
+        sum += vector[i];
+    }
+    util = (1.0 - (vector[3] - vector_[3]) / (double)(sum - sum_)) * 100.0;
+    sum_ = sum;
+    for (i = 0; i < ARRAY_SIZE(vector); ++i) {
+        vector_[i] = vector[i];
+    }
+    return util;
 }
 
-int
-main(int argc, char *argv[])
-{
-	const char * const PROC_STAT = "/proc/stat";
-	char line[1024];
-	FILE *file;
+int main(int argc, char *argv[]) {
+    const char *const PROC_STAT = "/proc/stat";
+    char line[1024];
+    FILE *file;
 
-	UNUSED(argc);
-	UNUSED(argv);
+    UNUSED(argc);
+    UNUSED(argv);
 
-	if (SIG_ERR == signal(SIGINT, _signal_)) {
-		TRACE("signal()");
-		return -1;
-	}
-	while (!done) {
-		if (!(file = fopen(PROC_STAT, "r"))) {
-			TRACE("fopen()");
-			return -1;
-		}
-		if (fgets(line, sizeof (line), file)) {
-			printf("\r%5.1f%%", cpu_util(line));
-			fflush(stdout);
-		}
-		us_sleep(500000);
-		fclose(file);
-	}
-	printf("\rDone!   \n");
-	return 0;
+    if (SIG_ERR == signal(SIGINT, _signal_)) {
+        TRACE("signal()");
+        return -1;
+    }
+    while (!done) {
+        if (!(file = fopen(PROC_STAT, "r"))) {
+            TRACE("fopen()");
+            return -1;
+        }
+        if (fgets(line, sizeof(line), file)) {
+            printf("\r%5.1f%%", cpu_util(line));
+            fflush(stdout);
+        }
+        us_sleep(500000);
+        fclose(file);
+    }
+    printf("\rDone!   \n");
+    return 0;
 }
